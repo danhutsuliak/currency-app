8d3f6fc0218626b68be6f86362dce538aefc152a
Daniel Hutsuliak <danguts23@gmail.com>
2022-10-23 16:07:52 +0300

    Move components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./components/header/header.component";
+import { HeaderComponent } from "../components/header/header.component";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
